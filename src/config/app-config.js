cf3248cb2b2e8ab7675491babcb64979b2651fdc
acchi17 <grf317@gmail.com>
2026-01-25 09:22:48 +0900

    Add missing files for entry execution feature

---
+++ src/config/app-config.js
@@ -0,0 +1,13 @@
+/**
+ * Application Configuration
+ */
+export default {
+  // Script execution settings
+  script: {
+    // Default language used throughout the application
+    engineName: 'javascript',
+    // Directory path for script files (relative to public directory root)
+    scriptsDir: '/public/scripts'
+  }
+  // Additional configuration categories can be added in the future
+}
